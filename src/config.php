bad4c7845319c6ab9fff633662c40bf5a9612221
forest-chan <71666648+forest-chan@users.noreply.github.com>
2022-03-14 14:57:32 +0300

    Update config.php

--- src/config.php
+++ src/config.php
@@ -15,7 +15,6 @@ $opt = [
 
 try{
     $db = new PDO("mysql:host={$config['host']};dbname={$config['dbname']}", $config['username'], $config['password'], $opt);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
     echo "An error occurs when connecting to db: ".$e->getMessage();
     exit();
